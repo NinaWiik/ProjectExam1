95c9bf53867ea50cbebebb85ee3ada1fd72980b7
Nina Wiik <ninawiik1@outlook.com>
2019-06-11 13:54:30 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 // INDEX
-fetch('http://api.open-notify.org/astros.json')
+fetch('https://cors-anywhere.herokuapp.com/http://api.open-notify.org/astros.json')
         .then(result => result.json())
     .then((res) => {
         createSpaceAPI(res);
@@ -11,7 +11,7 @@ function createSpaceAPI(result){
 }
 
 
-fetch('http://api.open-notify.org/iss-now.json')
+fetch('https://cors-anywhere.herokuapp.com/http://api.open-notify.org/iss-now.json')
         .then(result => result.json())
     .then((res) => {
         createSpaceAPI1(res);
@@ -36,7 +36,7 @@ function createRockets(result){
     for(var i=0; i < result.length; i++){
         document.getElementById('rockets').innerHTML += '<div class="rocketcontainer"><h2>' + result[i].rocket_name +' </h2> <img src="' + result[i].flickr_images[1] + '" class="rocketimages"> <p class="column"> Rocket ID: '+ result[i].rocket_id +' </p> <p class="column"> Rocket type: '+ result[i].rocket_type +' </p> <p class="column"> Height in meters: '+ result[i].height.meters + '  </p>  <p class="column"> Diameter in meters: '+ result[i].diameter.meters + ' </p>  <p class="column"> Mass in kilos: '+ result[i].mass.kg + '  </p> <p class="column">Cost per launch: '+ result[i].cost_per_launch +' </p> <p class="column">First Flight: '+ result[i].first_flight +' </p> <p class="column">Country: '+ result[i].country +' </p> <p class="column"> <p class="column"> Description: '+ result[i].description + '  </p>  <a class="links" href="' + result[i].wikipedia +' "> INFO ON WIKIPEDIA </a>  </div>';
     }
-};
+}
 
 // DRAGONS
 
@@ -52,7 +52,7 @@ function createDragons(result){
         document.getElementById('dragons').innerHTML += '<div class="rocketcontainer"><h2>' + result[i].name +' </h2> <img src="' + result[i].flickr_images[0] + '" class="rocketimages"> <p class="column"> Dragon ID: '+ result[i].id +' </p> <p class="column"> Type: '+ result[i].type +' </p> <p class="column"> Crew Capacity: '+ result[i].crew_capacity +' </p> <p class="column"> First Flight: '+ result[i].first_flight +' </p> <p class="column"> Dry Mass in Kg: '+ result[i].dry_mass_kg +' </p> <p class="column"> Diameter in meters: '+ result[i].diameter.meters +' </p> <p class="column"> Description: '+ result[i].description +' </p>  <a class="links" href="' + result[i].wikipedia +' "> INFO ON WIKIPEDIA </a> </div>';
     }
     
-};
+}
 
 // UPCOMING LAUNCHES
 
@@ -67,7 +67,7 @@ function createUpcomingLaunches(result){
     for(var i=0; i < result.length; i++){
         document.getElementById('upcominglaunches').innerHTML += '<div class="launchcontainer"><h5>' + result[i].mission_name +' </h5> <p class="column"> Flight Number: '+ result[i].flight_number +' </p> <p class="column"> Launch Year: '+ result[i].launch_year +' </p> <p class="column"> Launch date UTC: '+ result[i].launch_date_utc +' </p> <p class="column"> Rocket ID: '+ result[i].rocket.rocket_id +' </p> <p class="column"> Rocket Name: '+ result[i].rocket.rocket_name +' </p> <p class="column"> Launch Site: '+ result[i].launch_site.site_name_long +' </p> <p class="column"> Details: '+ result[i].details +' </p></div>'; 
     }
-};
+}
 
 
 // NEXT LAUNCH
@@ -97,6 +97,9 @@ function createLatestLaunch(result){
         document.getElementById('latestlaunch').innerHTML += '<div class="launchcontainer"><h5>' + result.mission_name +' </h5> <p class="column"> Flight Number: '+ result.flight_number +' </p> <p class="column"> Launch Success: '+ result.launch_success +' </p>  <p class="column"> Launch Year: '+ result.launch_year +' </p>  <p class="column"> Launch date UTC: '+ result.launch_date_utc +' </p> <p class="column"> Rocket ID: '+ result.rocket.rocket_id +' </p> <p class="column"> Rocket Name: '+ result.rocket.rocket_name +' </p> <p class="column"> Launch Site: '+ result.launch_site.site_name_long +' </p>  <p class="column"> Details: '+ result.details +' </p> <h3> LINKS </h3> <a class="linkslaunch" href="' + result.links.reddit_campaign +' "> Reddit Campaign </a> <a class="linkslaunch" href="' + result.links.reddit_launch +' "> Reddit Launch </a> <a class="linkslaunch" href="' + result.links.reddit_media +' "> Reddit Media </a> <a class="linkslaunch" href="' + result.links.presskit +' "> Press Kit </a> <a class="linkslaunch" href="' + result.links.article_link +' "> Article Link </a> <a class="linkslaunch" href="' + result.links.wikipedia +' "> Wikipedia </a> <a class="linkslaunch" href="' + result.links.video_link +' "> Youtube Video </a> <img src="' + result.links.flickr_images[2] + '" class="rocketimages"> </div>';
 }
 
+
+
+
 //CONTACT FORM
 document.getElementById("submitContact").addEventListener ("click", function () {
     var firstNameValue = document.getElementById("firstName").value;
@@ -104,14 +107,14 @@ document.getElementById("submitContact").addEventListener ("click", function () 
     document.getElementById("firstNameError").style.display = "none";
         if (!firstNameValue)  {
         document.getElementById("firstNameError").style.display = "block";
-        };
+        }
     
     var lastNameValue = document.getElementById("lastName").value;
     var lastNameError = document.getElementById("lastNameError");
         document.getElementById("lastNameError").style.display = "none";
         if (!lastNameValue) {
         document.getElementById("lastNameError").style.display = "block";
-        };
+        }
     
     
     var telephoneValue = document.getElementById("phone").value;
@@ -123,7 +126,7 @@ document.getElementById("submitContact").addEventListener ("click", function () 
     var phoneValidated = phoneValidation.test(telephoneValue);
         if (phoneValidation.test(telephoneValue) === false) {
             document.getElementById("phoneError").style.display = "block";
-        };
+        }
     
 
     var emailValue = document.getElementById("email").value; 
@@ -135,9 +138,9 @@ document.getElementById("submitContact").addEventListener ("click", function () 
     var emailValidated = emailValidation.test(emailValue);
         if (emailValidation.test(emailValue) === false) {
             document.getElementById("emailError").style.display = "block";
-        };
+        }
     
-}); 
+})
 
 
 // SPACE X SITE
@@ -154,4 +157,4 @@ document.getElementById("toggle").addEventListener("click", function() {
         displayText.display = "block";
         hidePicture.display = "none";
     }  
-});
+})
