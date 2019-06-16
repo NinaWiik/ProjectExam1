cf7c1dc33d000ea50a5046e420f5bcae2ac329b1
Nina Wiik <ninawiik1@outlook.com>
2019-06-16 13:38:22 +0200

    api
    added bold text to api

--- spacex.js
+++ spacex.js
@@ -6,7 +6,7 @@ fetch('https://cors-anywhere.herokuapp.com/http://api.open-notify.org/astros.jso
 .catch(err => console.log(err))
 
 function createSpaceAPI(result){
-    document.getElementById('spaceAPI').innerHTML += '<p class="apiInfo"> People in space: ' + result.number +' </p>';
+    document.getElementById('spaceAPI').innerHTML += '<b><p class="apiInfo"> People in space:</b> ' + result.number +' </p>';
 }
 
 
@@ -18,7 +18,7 @@ fetch('https://cors-anywhere.herokuapp.com/http://api.open-notify.org/iss-now.js
 .catch(err => console.log(err))
 
 function createSpaceAPI1(result){
-    document.getElementById('spaceAPI').innerHTML += '<h4>ISS Current position</h4> <p class="apiInfo"> Longitude: ' + result.iss_position.longitude +'</p><p class="apiInfo"> Latitude: ' + result.iss_position.latitude + '</p>';
+    document.getElementById('spaceAPI').innerHTML += '<h4>ISS Current position</h4><b> <p class="apiInfo"> Longitude:</b> ' + result.iss_position.longitude +'</p><b><p class="apiInfo"> Latitude: </b>' + result.iss_position.latitude + '</p>';
 }
 
 document.getElementById("toggle").addEventListener("click", function() {
